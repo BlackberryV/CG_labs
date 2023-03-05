d83ac8569c943097f854121bf65abf7650570837
venomusss <vovamushii@gmail.com>
2023-03-05 23:28:04 +0200

    feat: added tests to sphere class

---
+++ tests/sphere.test.ts
@@ -0,0 +1,46 @@
+import Ray from "../src/classes/ray/Ray";
+import Sphere from "../src/classes/sphere/Sphere";
+import Vector from "../src/classes/vector/Vector";
+
+describe('Sphere', () => {
+  const center = new Vector(0, 0, 0);
+  const radius = 3;
+  const sphere = new Sphere(center, radius);
+
+  describe('intersect', () => {
+    it('return true when ray intersects sphere', () => {
+      const origin = new Vector(4, 4, 4);
+      const direction = new Vector(-6, -5, -4);
+      const ray = new Ray(origin, direction);
+
+      expect(sphere.intersect(ray)).toBe(true);
+    });
+
+    it('return false when ray misses sphere', () => {
+      const origin = new Vector(5, 5, 5);
+      const direction = new Vector(10, 10, 10);
+      const ray = new Ray(origin, direction);
+
+      expect(sphere.intersect(ray)).toBe(false);
+    });
+  });
+
+  describe('getIntersections', () => {
+    it('return vectors of intersection (null)', () => {
+      const origin = new Vector(0, 6, -6);
+      const direction = new Vector(0, 0, 3);
+      const ray = new Ray(origin, direction);
+      console.log(sphere.getIntersections(ray))
+
+      expect(sphere.getIntersections(ray)).toBeNull();
+    });
+
+    it('should return two intersection points when ray intersects sphere twice', () => {
+        const origin = new Vector(0, 0, -6);
+        const direction = new Vector(0, 0, 3);
+        const ray = new Ray(origin, direction);
+  
+        expect(!!sphere.getIntersections(ray)?.length).toBeTruthy();
+      });
+  });
+});

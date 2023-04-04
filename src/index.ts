7dc03615481fb34b8aa04ef27fe54211d2df2381
Mykyta <nikitochkaaaaaa@gmail.com>
2023-04-04 12:37:35 +0300

    Task 1 normal input for 800x600

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import Disk from './classes/disk/Disk';
-import Plain from './classes/plain/Plain';
 import Sphere from './classes/sphere/Sphere';
 import Vector from './classes/vector/Vector';
 import Screen from "./classes/screen/Screen";
@@ -7,14 +5,16 @@ import Camera from "./classes/camera/Camera";
 import Raytracer from "./rayTrace/rayTracer";
 
 const camera = new Camera(30, new Vector(0, 0, 0));
-const screen = new Screen(100, 100);
-const objects = [
-  new Sphere(new Vector(0, 0, 1), 0.05),
-  new Sphere(new Vector(0.2, 0.2, 1), 0.2),
-  new Sphere(new Vector(-0.2, -0.2, 1), 0.2),
-]
-const lightDirection = new Vector(0, 0, -1).normalize()
-
+const screen = new Screen(800, 500);
 const rayTracer = new Raytracer(camera, screen);
-rayTracer.trace(objects, lightDirection);
+
+const objects = [
+  new Sphere(new Vector(0, 0, -5), 1),
+  new Sphere(new Vector(2, 2, -5), 1)
+]
+const lightDirection = new Vector(-1, 0, 0).normalize()
+
+const outputFile = 'output.ppm';
+
+rayTracer.trace(objects, lightDirection, outputFile);
 
